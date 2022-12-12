cc3b5f0188c8924923da29efa16dac0db5f151e7
Lomkile Ntetha <lumkilentetha878@gmail.com>
2022-12-12 18:01:28 +0200

    pushing caritas react

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+  api: {
+    API_URL: "http://102.130.118.244/",
+  }
+}
